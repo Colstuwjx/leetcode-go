c977440992103edf75eabd05ac89f7e1d98eff2e
Colstuwjx <Colstuwjx@gmail.com>
2018-05-26 17:49:43 +0800

    Fix race conditions.

--- 146_LRU_Cache/problem_146.go
+++ 146_LRU_Cache/problem_146.go
@@ -37,6 +37,12 @@ package main
    每次容量填满需要踢出数据时（即hashmap key not exist时），去掉最久没更新的一个item数据（即链表的头），并在尾部插入一条新数据；
 */
 
+import "log"
+
+const (
+	NotExistIndex = -1
+)
+
 type LinkedListItem struct {
 	index int
 	key   int
@@ -60,6 +66,10 @@ type LRUCache struct {
 func Constructor(capacity int) LRUCache {
 	var valueSlices []*LinkedListItem
 
+	if capacity <= 0 {
+		panic("capacity must be positive number!")
+	}
+
 	return LRUCache{
 		doublyLinkedList: &DoublyLinkedList{
 			head:   nil,
@@ -73,90 +83,162 @@ func Constructor(capacity int) LRUCache {
 
 func (this *LRUCache) Get(key int) int {
 	idx, exist := this.valueMap[key]
-	if !exist {
+	if !exist || idx == -1 {
 		return -1
 	}
 
-	return this.doublyLinkedList.values[idx].value
+	value := this.doublyLinkedList.values[idx].value
+	this.evictKeyWithAppendTail(idx, key, value)
+	return value
 }
 
-func (this *LRUCache) Iteral() map[int]int {
-	values := make(map[int]int)
-	for key, valueIdx := range this.valueMap {
-		values[key] = this.doublyLinkedList.values[valueIdx].value
+func (this *LRUCache) IndexMap() map[int]int {
+	return this.valueMap
+}
+
+func (this *LRUCache) Iteral() [][]int {
+	var values [][]int
+	pointer := this.doublyLinkedList.head
+	for {
+		if pointer == nil {
+			break
+		}
+
+		values = append(values, []int{pointer.key, pointer.value})
+		pointer = pointer.next
 	}
+
 	return values
 }
 
 func (this *LRUCache) evictKeyWithAppendTail(index, key, value int) *LRUCache {
-	var appendIndex int
+	item := &LinkedListItem{
+		index: index,
+		key:   key,
+		value: value,
+		prior: nil,
+		next:  nil,
+	}
 
-	// -1 means capacity is NOT full yet
-	if index == -1 {
-		appendIndex = len(this.valueMap)
-	} else {
-		// evict indexed item
+	// 1. head is nil or cap = 1
+	if this.doublyLinkedList.head == nil || this.capacity == 1 {
+		currentHead := this.doublyLinkedList.head
+		if currentHead != nil && currentHead.key != item.key {
+			this.valueMap[currentHead.key] = NotExistIndex
+		}
+
+		item.index = 0
+		this.doublyLinkedList.head = item
+		this.doublyLinkedList.tail = nil
+		if len(this.doublyLinkedList.values) != 0 {
+			this.doublyLinkedList.values[0] = item
+		} else {
+			this.doublyLinkedList.values = append(this.doublyLinkedList.values, item)
+		}
+
+		this.valueMap[key] = item.index
+		return this
+	}
+
+	// 2. head is not nil, and need to evict head
+	if this.doublyLinkedList.head.index == index {
+		// ONLY one item case
+		if this.doublyLinkedList.head.next == nil {
+			this.doublyLinkedList.head = item
+			this.doublyLinkedList.tail = nil
+
+			evictItemKey := this.doublyLinkedList.values[index].key
+			this.valueMap[evictItemKey] = NotExistIndex
+			this.doublyLinkedList.values[index] = item
+			this.valueMap[key] = index
+			return this
+		}
+
+		currentHead := this.doublyLinkedList.head
+		this.doublyLinkedList.head.next.prior = nil
+		this.doublyLinkedList.head = currentHead.next
+		this.doublyLinkedList.tail.next = item
+		item.prior = this.doublyLinkedList.tail
+		this.doublyLinkedList.tail = item
+
+		evictHeadKey := this.doublyLinkedList.values[index].key
+		this.valueMap[evictHeadKey] = NotExistIndex
+		this.doublyLinkedList.values[index] = item
+		this.valueMap[key] = item.index
+		return this
+	}
+
+	// 3. tail is nil and need to init
+	if this.doublyLinkedList.tail == nil {
+		this.doublyLinkedList.head.next = item
+		this.doublyLinkedList.tail = item
+		this.doublyLinkedList.tail.prior = this.doublyLinkedList.head
+		this.doublyLinkedList.values = append(this.doublyLinkedList.values, item)
+		this.valueMap[key] = index
+		return this
+	}
+
+	// 4. evict other node(include tail), or append item
+	if len(this.doublyLinkedList.values) > index {
 		evictItem := this.doublyLinkedList.values[index]
-		if evictItem.prior != nil {
+		if evictItem.prior.next != nil {
 			evictItem.prior.next = evictItem.next
+		} else {
+			this.doublyLinkedList.head.next = item
 		}
 
 		if evictItem.next != nil {
 			evictItem.next.prior = evictItem.prior
+		} else {
+			this.doublyLinkedList.tail = evictItem.prior
 		}
-
-		// head has been replaced, reset head
-		if evictItem.index == this.doublyLinkedList.head.index {
-			this.doublyLinkedList.head = this.doublyLinkedList.head.next
-		}
-
-		// reuse the evict item index
-		appendIndex = index
-
-		// clear evict item's key-value mapping
-		// FIXME: delete operation is heavy, should be marked as unused
-		delete(this.valueMap, evictItem.key)
+		this.valueMap[evictItem.key] = NotExistIndex
 	}
 
-	newItem := &LinkedListItem{
-		index: appendIndex,
-		value: value,
-		key:   key,
-		prior: this.doublyLinkedList.tail,
-		next:  nil,
-	}
-
-	if index == -1 {
-		this.doublyLinkedList.values = append(this.doublyLinkedList.values, newItem)
-
-		// init head if it is nil
-		if this.doublyLinkedList.head == nil {
-			this.doublyLinkedList.head = newItem
-		}
+	item.prior = this.doublyLinkedList.tail
+	this.doublyLinkedList.tail.next = item
+	this.doublyLinkedList.tail = item
+	if len(this.doublyLinkedList.values) == index {
+		this.doublyLinkedList.values = append(this.doublyLinkedList.values, item)
 	} else {
-		this.doublyLinkedList.values[appendIndex] = newItem
+		this.doublyLinkedList.values[index] = item
 	}
 
-	// rotate tail if it exists
-	if this.doublyLinkedList.tail != nil {
-		this.doublyLinkedList.tail.next = newItem
-	}
-
-	this.doublyLinkedList.tail = newItem
-	this.valueMap[key] = appendIndex
+	this.valueMap[key] = item.index
 	return this
 }
 
 func (this *LRUCache) Put(key int, value int) {
 	idx, exist := this.valueMap[key]
-	if !exist {
-		if len(this.valueMap) == this.capacity {
+	if !exist || idx == NotExistIndex {
+		if len(this.doublyLinkedList.values) >= this.capacity {
 			evictHeadIndex := this.doublyLinkedList.head.index
 			this.evictKeyWithAppendTail(evictHeadIndex, key, value)
 		} else {
-			this.evictKeyWithAppendTail(-1, key, value)
+			appendToTailIndex := len(this.doublyLinkedList.values)
+			this.evictKeyWithAppendTail(appendToTailIndex, key, value)
 		}
 	} else {
 		this.evictKeyWithAppendTail(idx, key, value)
 	}
 }
+
+// func main() {
+// 	cache := Constructor(1)
+// 	log.Println("Put (2, 1)")
+// 	cache.Put(2, 1)
+
+// 	log.Println("Geting 2")
+// 	log.Println("Get 2: ", cache.Get(2))
+
+// 	log.Println("Put (3, 2)")
+// 	cache.Put(3, 2)
+
+// 	log.Println("Geting 2")
+// 	log.Println("Get 2: ", cache.Get(2))
+
+// 	log.Println("Geting 3")
+// 	log.Println("Get 3: ", cache.Get(3))
+
+// 	log.Println("Test passed!")
+// }
